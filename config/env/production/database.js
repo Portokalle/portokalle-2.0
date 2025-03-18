8758d55cbaf17ac32521974ddad510935a3d157e
lazar03 <alfonzlazri@gmail.com>
2025-03-18 01:27:10 +0100

    fixes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,16 +2,17 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        host: env('DATABASE_HOST'), // This will be the URL or IP from DigitalOcean
+        port: env.int('DATABASE_PORT', 5432), // Default PostgreSQL port
+        database: env('DATABASE_NAME', 'strapi'), // Default database name
+        user: env('DATABASE_USERNAME', 'strapi'), // Default username
+        password: env('DATABASE_PASSWORD', 'strapi'), // Default password
+        schema: env('DATABASE_SCHEMA', 'public'), // Optional schema
         ssl: {
-          ca: env('DATABASE_CA') // For self-signed certificates
+          ca: env('DATABASE_CA') // For self-signed certificates if needed
         },
       },
       debug: false,
     },
   });
+  
